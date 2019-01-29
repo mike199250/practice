b225ce86b8ed6eba8ba4bc372bca84659725af8c
Unknown <mike199250@gmail.com>
2019-01-29 17:21:40 +0800

    002 Roll a ball - 07 Displaying the Score and Text
    Create UI toolset and texts
    Fix indent to tab

--- p/002_Roll_a_Ball/Roll a Ball/Assets/Scripts/CameraController.cs
+++ p/002_Roll_a_Ball/Roll a Ball/Assets/Scripts/CameraController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+	public GameObject player;
 
-    private Vector3 offset;
+	private Vector3 offset;
 
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+	void Start()
+	{
+		offset = transform.position - player.transform.position;
+	}
 
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
 }
