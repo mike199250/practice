c9f2804fbc5e24c677dff3bf871a0b64e797d9f0
Unknown <mike199250@gmail.com>
2019-07-05 16:53:26 +0800

    006 Lights Out - Add search
    enumerate 2^col switch of first row
    chase down
    check if same with target

--- p/006_Lights_Out/scripts/main.js
+++ p/006_Lights_Out/scripts/main.js
@@ -2,6 +2,8 @@ var flip_adjacent = false;
 var id_table = ["t0","t1","t2"];
 var light_color =["#ffff99","#ffff99","#b3ffb3"];
 var row,col;
+var map_beg,map_end,map_cur,map_ans;
+
 
 function init(r,c){
 	var i,j,k;
@@ -24,6 +26,13 @@ function init(r,c){
 		document.getElementById(id[k]).innerHTML = s;
 	}
 	//console.log(s);
+	for(i=0;i<row;i++){
+		for(j=0;j<col;j++){
+			flip_cell(0,i,j);
+		}
+	}
+
+	document.getElementById("status").innerHTML = "result:";
 }
 function change_flip_adjacent(){
 	flip_adjacent = document.getElementById("flip_adjacent").checked;
@@ -46,5 +55,104 @@ function flip(k,i,j){
 		if(i<row-1)flip_cell(k,i+1,j);
 		if(j>0)flip_cell(k,i,j-1);
 		if(j<col-1)flip_cell(k,i,j+1);
+	}
+}
+
+function search(){
+	var t=[];
+	var i,j,k;
+	
+	t[0] = document.getElementById(id_table[0]);
+	t[1] = document.getElementById(id_table[1]);
+	t[2] = document.getElementById(id_table[2]);
+	
+	map_beg=[];
+	map_end=[];
+	map_cur=[];
+	map_ans=[];
+	for(i=0;i<row;i++){
+		map_beg[i]=[];
+		map_end[i]=[];
+		map_cur[i]=[];
+		map_ans[i]=[];
+		for(j=0;j<col;j++){
+			if(t[0].rows[i].cells[j].bgColor==""){
+				map_beg[i][j] = 0;
+			}else{
+				map_beg[i][j] = 1;
+			}
+			if(t[1].rows[i].cells[j].bgColor==""){
+				map_end[i][j] = 0;
+			}else{
+				map_end[i][j] = 1;
+			}
+			if(t[2].rows[i].cells[j].bgColor!=""){
+				t[2].rows[i].cells[j].bgColor="";
+			}
+			map_ans[i][j]=0;
+		}
+	}
+
+	if(enumerate(0)){
+		document.getElementById("status").innerHTML="solution found";
+		for(i=0;i<row;i++){
+			for(j=0;j<col;j++){
+				if(map_ans[i][j]){
+					flip_cell(2,i,j);
+				}
+			}
+		}
+	}else{
+		document.getElementById("status").innerHTML="no solution found";
+	}
+	
+}
+function invert_cell(i,j){
+	map_cur[i][j]^=1;
+}
+function invert(i,j){
+	invert_cell(i,j);
+	if(i>0)invert_cell(i-1,j);
+	if(i<row-1)invert_cell(i+1,j);
+	if(j>0)invert_cell(i,j-1);
+	if(j<col-1)invert_cell(i,j+1);
+}
+function enumerate(deep){
+	if(deep==col){
+		//console.log(map_ans[0]);
+		var i,j;
+		for(i=0;i<row;i++){
+			for(j=0;j<col;j++){
+				map_cur[i][j] = map_beg[i][j];
+			}
+		}
+		for(j=0;j<col;j++){
+			if(map_ans[0][j]==1)invert(0,j);
+		}
+		for(i=1;i<row;i++){
+			for(j=0;j<col;j++){
+				if(map_cur[i-1][j] != map_end[i-1][j]){
+					invert(i,j);
+					map_ans[i][j]=1;
+				}else{
+					map_ans[i][j]=0;
+				}
+			}
+		}
+		for(j=0;j<col;j++){
+			if(map_cur[row-1][j] != map_end[row-1][j]){
+				return false;
+			}
+		}
+		//console.log(map_ans);
+		return true;
+	}else{
+		map_ans[0][deep] = 0;
+		if(enumerate(deep+1))
+			return true;
+		map_ans[0][deep] = 1;
+		if(enumerate(deep+1))
+			return true;
+		return false;
 	}
 }
